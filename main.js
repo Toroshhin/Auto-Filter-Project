c98ab1b73a1eadfabcfb2501799ce4011158604f
Toroshhin <https://github.com/Toroshhin>
2020-03-03 15:22:31 +0300

    Added post output

--- main.js
+++ main.js
@@ -1,22 +1,93 @@
 window.onload = function () {
-  let car = [
-    { brand: "Land Rover", modelArr: [{ model: "Defender", modification: ["I", "I Рестайлинг", "II"] }, { model: "Discovery", modification: ["L318", "L319", "L462"] }, { model: "Freelander", modification: ["I", "I Рестайлинг", "II", "II Рестайлинг", "II Рестайлинг 2"] }, { model: "Evoque", modification: ["I", "I Рестайлинг", "II"] }, { model: "Range Rover Sport", modification: ["L320", "L494"] }] },
-    { brand: "Nissan", modelArr: [{ model: "Almera", modification: ["I (N15)", "II (N16)", "II (N16) Рестайлинг", "III (G15"] }, { model: "Pathfinder", modification: ["I", "II", "III", "III Рестайлинг", "IV", "IV Рестайлинг"] }, { model: "Qashqai", modification: ["J10", "J11"] }, { model: "X-Trail", modification: ["T30", "T31", "T32"] }] },
-    { brand: "Toyota", modelArr: [{ model: "Camry", modification: ["V10", "V20", "V30", "V40", "V50", "XV40", "XV50"] }, { model: "Corolla", modification: ["E120", "E150", "E160", "E210"] }, { model: "Crown", modification: ["S120", "S140", "S150", "S170", "S180", "S200", "S210"] }, { model: "Land Cruiser", modification: ["80 Series", "100 Series", "200 Series",] }] },
-    { brand: "Volkswagen", modelArr: [{ model: "Golf", modification: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"] }, { model: "Jetta", modification: ["5 Поколение", "6 Поколение", "6 Поколение Рестайлинг", "7 Поколение"] }, { model: "Passat", modification: ["B3", "B4", "B5", "B6", "B7", "B8,5", "B8"] }, { model: "Polo", modification: ["Mk2", "Mk3", "Mk4", "Mk5"] }, { model: "Tiguan", modification: ["I", "I Рестайлинг", "II"] }] },];
-  let carBrands = document.querySelector('#brand');
-  let classCarBrands = "brend-option";
+  const car = [
+    { brand: "Land Rover", models: [{ model: "Defender", modification: ["90", "110", "130"], photo: ["https://s.auto.drom.ru/i24236/c/photos/generations/500x_land_rover_defender_g9000.jpg?910136"] }, { model: "Discovery", modification: ["L318", "L319", "L462"] }, { model: "Freelander", modification: ["I", "I Рестайлинг", "II", "II Рестайлинг", "II Рестайлинг 2"] }, { model: "Evoque", modification: ["I", "I Рестайлинг", "II"] }, { model: "Range Rover Sport", modification: ["L320", "L494"] }] },
+    { brand: "Nissan", models: [{ model: "Almera", modification: ["I (N15)", "II (N16)", "II (N16) Рестайлинг", "III (G15"] }, { model: "Pathfinder", modification: ["I", "II", "III", "III Рестайлинг", "IV", "IV Рестайлинг"] }, { model: "Qashqai", modification: ["J10", "J11"] }, { model: "X-Trail", modification: ["T30", "T31", "T32"] }] },
+    { brand: "Toyota", models: [{ model: "Camry", modification: ["V10", "V20", "V30", "V40", "V50", "XV40", "XV50"] }, { model: "Corolla", modification: ["E120", "E150", "E160", "E210"] }, { model: "Crown", modification: ["S120", "S140", "S150", "S170", "S180", "S200", "S210"] }, { model: "Land Cruiser", modification: ["80 Series", "100 Series", "200 Series",] }] },
+    { brand: "Volkswagen", models: [{ model: "Golf", modification: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"] }, { model: "Jetta", modification: ["5 Поколение", "6 Поколение", "6 Поколение Рестайлинг", "7 Поколение"] }, { model: "Passat", modification: ["B3", "B4", "B5", "B6", "B7", "B8,5", "B8"] }, { model: "Polo", modification: ["Mk2", "Mk3", "Mk4", "Mk5"] }, { model: "Tiguan", modification: ["I", "I Рестайлинг", "II"] }] },];
+  let carBrands = document.querySelector("#brand");
+  let carModels = document.querySelector("#model");
+  let carMod = document.querySelector("#modification");
 
-  function addOption(className, optionName) {
+
+
+
+  //Вывод данных из БД в select
+  function addOption(brand, model, mod, k, j) {
+    //Вывод брендов в select
     for (let i = 0; i < car.length; i++) {
-      let option = document.createElement('option');
-      className.append(option);
-      option.className = optionName;
+      let option = document.createElement("option");
+      brand.append(option);
+      option.setAttribute("name", i);
       option.innerHTML = car[i].brand;
+    }
+    //Вывод моделей в select
+    for (let i = 0; i < car[k].models.length; i++) {
+      let option = document.createElement("option");
+      model.append(option);
+      option.setAttribute("name", i);
+      option.innerHTML = car[k].models[i].model;
+    }
+    //Вывод модицикаций в select
+    for (let i = 0; i < car[k].models[j].modification.length; i++) {
+      let option = document.createElement("option");
+      mod.append(option);
+      option.setAttribute("name", i);
+      option.innerHTML = car[k].models[j].modification[i];
     }
   }
 
+  function upduteUI(k, j) {
+    //Удаление постов
+    let advLength = document.getElementsByClassName("advert").length
+    for (let i = 0; i < advLength; i++) {
+      let advert = document.querySelector(".advert");
+      advert.remove();
+    }
+
+    for (i = 0; i < car[k].models[j].modification.length; i++) {
+
+      //Добавление поста
+      let ads = document.querySelector(".ads");
+      let div = document.createElement("div");
+      div.className = "advert";
+      ads.append(div);
+
+      //Добавление картинки в пост
+      let img = document.createElement("img");
+      img.className = "advert__img"
+      img.src = car[0].models[0].photo[0];
+      div.append(img);
+
+      //Добавление бренда в пост
+      var p = document.createElement("p");
+      p.className = "advert__brand"
+      p.innerHTML = car[k].brand;
+      div.append(p);
+
+      //Добавление модели в пост
+      var p = document.createElement("p");
+      p.className = "advert__brand"
+      p.innerHTML = car[k].models[j].model;
+      div.append(p);
+
+      //Добавление модификации в пост
+      var span = document.createElement("span");
+      span.className = "advert_modification"
+      span.innerHTML = " (" + car[k].models[j].modification[i] + ")";
+      p.append(span);
+    }
+
+  }
+
+  upduteUI(0, 0);
 
 
-  addOption(carBrands, classCarBrands);
+
+
+
+
+  let k = 3;
+  let j = 2;
+
+  addOption(carBrands, carModels, carMod, k, j);
 }
